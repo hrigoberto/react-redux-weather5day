1894d7ce24abe1511fb8273b3677358fe240e451
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-11-06 14:26:41 -0800

    making full axios request, now have to pass the action through the reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
+import axious from 'axios';
+
 const API_KEY = '5c3b8acf089987c1a9a681f030fb3fcd';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
