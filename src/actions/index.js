1745d060c8b328f92b0e6f7a48a6c9e774ba640d
jfkbeatkid <hernadezrigoberto@gmail.com>
2018-11-06 14:50:33 -0800

    successful aajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
-import axious from 'axios';
+import axios from 'axios';
 
 const API_KEY = '5c3b8acf089987c1a9a681f030fb3fcd';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
@@ -9,6 +9,7 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`
   const request = axios.get(url);
 
+  console.log('REQUEST:', request);
   return {
     type: FETCH_WEATHER,
     payload: request
